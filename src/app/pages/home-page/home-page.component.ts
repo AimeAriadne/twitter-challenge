69dc3241bae3c769363dbb74a3a60792068b6021
Aime Ramalho <aimeariadne@hotmail.com>
2023-06-28 16:54:28 +0100

    add global styles and structure home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
+  faStar = faStar;
 }
