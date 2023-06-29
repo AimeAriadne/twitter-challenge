5b1b6eb90844918bd8e8c48d464507eb31dbd368
Aime Ramalho <aimeariadne@hotmail.com>
2023-06-29 09:16:52 +0100

    create tweet and user interfaces

---
+++ src/app/itweet.ts
@@ -0,0 +1,7 @@
+export default interface ITweet {
+  user: string,
+  image: string,
+  account: string,
+  time: number,
+  tweet: string
+}
---
+++ src/app/iuser.ts
@@ -0,0 +1,5 @@
+export default interface IUser {
+    name: string,
+    account: string,
+    image: string,
+}
